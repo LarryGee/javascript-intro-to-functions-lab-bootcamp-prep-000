2b0f0ddc820e3648bda4865adc4906771b049bfa
Larry Jr <larry.g3284@yahoo.com>
2018-02-02 22:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
+<<<<<<< HEAD
   } else if (string === "I love you, Grandma.") {
+=======
+  } else if (string === "I love you, Grandma") {
+>>>>>>> 4313288d1d15f7dc156339b96fbbb51096c7e9e3
     return "I love you, too."
   }
 }
